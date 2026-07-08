bb45752c6155915c92b68ab9489f55755c5bf7b6
Pedro Henrique Contardi Soler <pedrocsoler@hotmail.com>
2026-07-08 16:54:03 -0300

    test(shell): adapta teste do shell para abas reais de ganhos e despesas

--- test/main_shell_test.dart
+++ test/main_shell_test.dart
@@ -2,13 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:driveflow/core/constants/app_constants.dart';
 import 'package:driveflow/core/theme/app_theme.dart';
 import 'package:driveflow/features/authentication/domain/entities/user_entity.dart';
 import 'package:driveflow/features/authentication/presentation/providers/auth_providers.dart';
 import 'package:driveflow/features/dashboard/presentation/screens/main_shell_screen.dart';
+import 'package:driveflow/features/earnings/presentation/providers/earnings_providers.dart';
+import 'package:driveflow/features/expenses/presentation/providers/expenses_providers.dart';
 import 'package:driveflow/features/profile/presentation/providers/profile_providers.dart';
-import 'package:driveflow/features/vehicle/domain/entities/vehicle_entity.dart';
 import 'package:driveflow/features/vehicle/presentation/providers/vehicle_providers.dart';
 import 'package:driveflow/shared/widgets/driveflow_bottom_nav_bar.dart';
 
@@ -29,19 +29,9 @@ void main() {
               email: 'd@test.com',
             ),
           ),
-          vehiclesStreamProvider.overrideWith(
-            (ref) => Stream.value([
-              const VehicleEntity(
-                id: 'v1',
-                userId: 'u1',
-                brand: 'Fiat',
-                model: 'Argo',
-                year: 2021,
-                fuel: FuelType.flex,
-                odometerKm: 30000,
-              ),
-            ]),
-          ),
+          earningsStreamProvider.overrideWith((ref) => Stream.value(const [])),
+          expensesStreamProvider.overrideWith((ref) => Stream.value(const [])),
+          vehiclesStreamProvider.overrideWith((ref) => Stream.value(const [])),
         ],
         child: MaterialApp(
           theme: buildDriveFlowDarkTheme(),
@@ -59,7 +49,7 @@ void main() {
     await tester.pump();
     await tester.pump(const Duration(milliseconds: 300));
 
-    expect(find.textContaining('Registre corridas por plataforma'), findsOneWidget);
+    expect(find.text('Ganhos'), findsOneWidget);
 
     await tester.tap(find.text('PERFIL'));
     await tester.pump();
