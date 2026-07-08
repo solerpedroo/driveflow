d0fafafda337d0e7e7a152c87888db5e781ea5c6
Pedro Henrique Contardi Soler <pedrocsoler@hotmail.com>
2026-07-08 16:45:35 -0300

    feat(vehicle): define contrato abstrato VehicleRepository

---
+++ lib/features/vehicle/domain/repositories/vehicle_repository.dart
@@ -0,0 +1,17 @@
+import '../entities/vehicle_entity.dart';
+
+/// Contrato de persistência de veículos.
+abstract interface class VehicleRepository {
+  Stream<List<VehicleEntity>> watchVehicles();
+
+  Future<List<VehicleEntity>> fetchVehicles();
+
+  Future<VehicleEntity> createVehicle(VehicleDraft draft);
+
+  Future<VehicleEntity> updateVehicle({
+    required String id,
+    required VehicleDraft draft,
+  });
+
+  Future<void> deleteVehicle(String id);
+}
