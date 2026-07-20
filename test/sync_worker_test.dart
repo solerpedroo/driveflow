2bd94f859ba7d25e6c746acd1061e4c9d6b199b8
Pedro Henrique Contardi Soler <pedrocsoler@hotmail.com>
2026-07-20 12:35:15 -0300

    test(sync): inicializa Supabase mockado e Hive criptografado

--- test/sync_worker_test.dart
+++ test/sync_worker_test.dart
@@ -1,15 +1,16 @@
 import 'dart:io';
 
 import 'package:flutter_test/flutter_test.dart';
-import 'package:hive/hive.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'package:driveflow/core/services/connectivity_service.dart';
 import 'package:driveflow/core/services/sync_status.dart';
 import 'package:driveflow/core/services/sync_worker.dart';
-import 'package:driveflow/core/storage/hive_boxes.dart';
 import 'package:driveflow/core/storage/local_entity_cache.dart';
 import 'package:driveflow/core/storage/pending_sync_operation.dart';
 import 'package:driveflow/core/storage/pending_sync_queue.dart';
+import 'support/hive_test_helper.dart';
 
 class _FakeConnectivity extends ConnectivityService {
   _FakeConnectivity(this._online);
@@ -26,22 +27,28 @@ class _FakeConnectivity extends ConnectivityService {
 }
 
 void main() {
-  late Directory tempDir;
   late LocalEntityCache cache;
   late PendingSyncQueue queue;
+  late Directory tempDir;
+
+  setUpAll(() async {
+    TestWidgetsFlutterBinding.ensureInitialized();
+    SharedPreferences.setMockInitialValues({});
+    await Supabase.initialize(
+      url: 'http://127.0.0.1:54321',
+      anonKey: 'test-anon-key',
+    );
+  });
 
   setUp(() async {
     tempDir = await Directory.systemTemp.createTemp('driveflow_sync_worker_');
-    Hive.init(tempDir.path);
-    for (final name in HiveBoxes.all) {
-      await Hive.openBox<dynamic>(name);
-    }
+    await openHiveBoxesForTests(path: tempDir.path);
     cache = LocalEntityCache();
     queue = PendingSyncQueue(cache: cache);
   });
 
   tearDown(() async {
-    await Hive.close();
+    await closeHiveBoxesForTests();
     if (tempDir.existsSync()) {
       await tempDir.delete(recursive: true);
     }
