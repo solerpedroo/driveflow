c4f6daafddc977846d7017cd2ec753c757b92251
Pedro Henrique Contardi Soler <pedrocsoler@hotmail.com>
2026-07-20 12:35:14 -0300

    test(storage): usa Hive criptografado no helper de testes

--- test/offline_storage_test.dart
+++ test/offline_storage_test.dart
@@ -1,12 +1,12 @@
 import 'dart:io';
 
 import 'package:flutter_test/flutter_test.dart';
-import 'package:hive/hive.dart';
 
 import 'package:driveflow/core/storage/hive_boxes.dart';
 import 'package:driveflow/core/storage/local_entity_cache.dart';
 import 'package:driveflow/core/storage/pending_sync_operation.dart';
 import 'package:driveflow/core/storage/pending_sync_queue.dart';
+import 'support/hive_test_helper.dart';
 
 void main() {
   late Directory tempDir;
@@ -15,16 +15,13 @@ void main() {
 
   setUp(() async {
     tempDir = await Directory.systemTemp.createTemp('driveflow_hive_');
-    Hive.init(tempDir.path);
-    for (final name in HiveBoxes.all) {
-      await Hive.openBox<dynamic>(name);
-    }
+    await openHiveBoxesForTests(path: tempDir.path);
     cache = LocalEntityCache();
     queue = PendingSyncQueue(cache: cache);
   });
 
   tearDown(() async {
-    await Hive.close();
+    await closeHiveBoxesForTests();
     if (tempDir.existsSync()) {
       await tempDir.delete(recursive: true);
     }
